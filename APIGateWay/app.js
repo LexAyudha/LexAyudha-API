8d0de79e144aa90b5812a269bc9e6d2cb370c0ac
X-pose <falconbusiness123@gmail.com>
2024-10-09 21:28:32 +0530

    APIGateWay implemented. Initial commit

---
+++ APIGateWay/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+const proxy = require("express-http-proxy");
+const { applyRateLimiter } = require("./utilis/rateLimiter");
+const { authenticateRequest } = require("./utilis/requestAuthenticator");
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+//host.docker.internal
+app.use("/api/auth", applyRateLimiter, proxy("host.docker.internal:8001"));
+
+//Exporting app to be used by the server.js
+module.exports = app;
